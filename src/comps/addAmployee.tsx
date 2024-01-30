2402e1515f53ed20461ae05fdb58d067e1b44784
MartinKruess <martin.kruss@digitalcareerinstitute.org>
2024-01-30 09:09:15 +0100

    get data from form

--- src/comps/addAmployee.tsx
+++ src/comps/addAmployee.tsx
@@ -6,15 +6,15 @@ import { ShowEmployees } from "./AddEmloyeeAssets/showEmployees";
 import { EmployeeStateType } from "./global/typeManager";
 
 export const AddEmployee = () => {
-  const [employees, setEmployees] = useState([]) as EmployeeStateType;
+  const [employees, setEmployees] = useState<any>([]);
   const num = 1;
 
   return (
     <div>
       <h1>Landing Page</h1>
       <p>Lege {num} Mitarbeiter an.</p>
-      <AddEmployeeForm setEmployees={setEmployees} />
-      {/* <ShowEmployees employees={employees} /> */}
+      <AddEmployeeForm employees={employees} setEmployees={setEmployees} />
+      <ShowEmployees employees={employees} setEmployees={setEmployees} />
     </div>
   );
 };
